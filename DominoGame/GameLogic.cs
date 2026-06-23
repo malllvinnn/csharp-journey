2bbcf4d8b55ffb43468c64809d262f7fa4a0ce30
MUHAMMAD MALFIN <malvin.arkade7@gmail.com>
2026-06-23 09:40:12 +0700

    implementasi kerangka method (belum logic bisnis) yang ada

---
+++ DominoGame/GameLogic.cs
@@ -0,0 +1,84 @@
+using DominoGame.Enums;
+using DominoGame.Interfaces;
+
+namespace DominoGame;
+
+public class GameLogic
+{
+    private Dictionary<IPlayer, List<IDomino>> _hands;
+    private List<IPlayer> _players;
+    private IBoard _board;
+    private IPlayer _winner;
+    private IDrawPile _drawPile;
+    private int _currentPlayerIndex;
+    private int _consecutiveSkips;
+    private GameStatus _gameStatus;
+    private WinCondition _winCondition;
+    
+    // events
+    public event EventHandler TurnChanged;
+    public event EventHandler GameEnded;
+
+    public GameLogic(List<IPlayer> players, IDrawPile drawPile)
+    {
+        _players = players;
+        _drawPile = drawPile;
+    }
+    
+    // Lifecycle (public)
+    public void StartGame(){}
+    public IPlayer GetWinner(){}
+    public WinCondition GetWinConditions(){}
+    
+    // Actions (public)
+    public TurnAction PlayTurn(
+        IPlayer player, 
+        IDomino domino, 
+        PlacementSide side,
+        PlacementOrientation orientation
+    ){}
+    public void DrawCard(IPlayer player){}
+    public void AdvanceTurn(){}
+    
+    // Get Info Game and Turn
+    public GameStatus GetStatus(){}
+    public IPlayer GetCurrentPlayer(){}
+    
+    // Get Info Player and Hand
+    public List<IPlayer> GetPlayers(){}
+    public List<IDomino> GetPlayerHands(IPlayer player){}
+    public int GetRemainingPips(IPlayer player){}
+    
+    // Get Info Board
+    public IBoard GetBoard(){}
+    public List<int> GetOpenEndPips(){}
+    public int GetDrawPileCount(){}
+    
+    // Business Logic Setup (Private)
+    private void ShuffleDrawPile(){}
+    private void DealInitialHands(){}
+    private void DetermineStartingPlayer(){}
+    private IDomino GetHighestDouble(IPlayer player){}
+    private bool HasAnyDouble(){}
+    private void RedealAllHands(){}
+    
+    // Business Logic Turn and Placement (Private)
+    private bool ValidatePlacement(IDomino domino, PlacementSide side){}
+    private void PlaceDomino(IDomino domino, PlacementSide side, PlacementOrientation orientation){}
+    private bool HasPlayableDomino(IPlayer player){}
+    
+    // Business Logic Pile and Hand (Private)
+    private IDomino DrawFromPile(){}
+    private void AddDominoToHand(IPlayer player, IDomino domino){}
+    private void RemoveDominoFromHand(IPlayer player, IDomino domino){}
+    private void DrawUntilPlayable(IPlayer player){}
+    
+    // Business Logic Game End and Resolution (Private)
+    private bool CheckGameOver(){}
+    private bool IsGameBlocked(){}
+    private bool IsDrawPileEmpty(){}
+    private int CalculateRemainingPips(IPlayer player){}
+    private IPlayer DetermineWinner(){}
+    private void EndGame(){}
+    
+}
